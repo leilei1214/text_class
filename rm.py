01f1dd732a0c8076bbe2838d3170a83372ab141b
leilei1214 <37176878+leilei1214@users.noreply.github.com>
2022-04-09 16:03:03 +0800

    Create rm.py

---
+++ rm.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+pinRelay = 17
+pin1 = 7
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pin1,GPIO.IN,pull_down=GPIO.PUD_up)
+GPIO.setup(pinRelay,GPIO.OUT)
+n=1
+def event(pin):
+	global n
+	GPIO.remove_event_detect(pin)
+	print('按了{}'.format(n))
+	n +=1
+	GPIO.output(pinRelay,not GPIO.inpue(pin1))
+	GPIO.add_event_detect(pin,GPIO.FALLING,callback =event,bouncetime=1500)
+try:
+	GPIO.add_event_detect(pin,GPIO.FALLING,callback =event,bouncetime=1500)
+	while True:
+		time.sleep(1000000)
+except KeyboardInterrupt:
+	pass
+except Exception as err:
+	print(err)
+GPIO.cleanup()
